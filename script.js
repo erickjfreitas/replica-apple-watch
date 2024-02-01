38302f96f7e3f0330fcb6fb90ce8af8b1912608e
erickjfreitas <erickstar10@gmail.com>
2024-01-31 21:00:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const verdeCipreste = {
+    nome:'Verde-cipreste',
+    pasta:'imagens-verde-cipreste',
+}
+
+const meiaNoite = {
+    nome:'meia-noite',
+    pasta:'imagens-meia-noite',
+}
+
+const estelar = {
+    nome:'Estelar',
+    pasta:'imagens-estelar',
+}
+
+const azulInverno = {
+    nome:'Azul-inverno',
+    pasta:'imagens-azul-inverno',
+}
+
+const rosaClaro = {
+    nome:'Rosa-claro',
+    pasta:'imagens-rosa-claro',
+}
+
+const minhalista = [verdeCipreste, azulInverno, meiaNoite, estelar, rosaClaro]
+
+
